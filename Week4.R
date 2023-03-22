14dbd238bac047c9a0ae9ddac04f667e43175f20
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2023-03-23 11:50:13 +1300

    quick

---
+++ Week4.R
@@ -0,0 +1,46 @@
+airlines <- read.csv(file = "data202/airlines.csv")
+
+# exact Matching of strings
+airlines[airlines$Country == "New Zealand"]
+
+# Match substrings
+c <- "New Zealand"
+substring(c, 2, 2)
+nchar(c)
+
+tolower(c)
+toupper("New Zealand")
+
+idx.thailand <- airlines$Country == "Thailand"
+airlines[idx.thailand,]
+
+sum(idx.thailand) # 54
+
+thai.airlines <- airlines[idx.thailand, ]
+dim(thai.airlines)
+
+View(thai.airlines)
+
+sum(tolower(airlines$Country)=="thailand")
+airlines$Country[tolower(airlines$Country)=="thailand"]
+
+# find "Royal" at the START of an airline name
+roy1 <- airlines[substring(airlines$Airline, 1, 5) == "Royal", ]
+dim(roy1) # 37 rows
+
+# find "Royal" ANYWHERE in an airline name
+x <- c("Wilma", "Fred")
+grep("r", x) # give the locations
+
+x[grep("r", x)]
+
+# a vevctor of integer locations
+grep("royal", tolower(airlines$Airline))
+
+# a vector of TRUE/FALSE
+grepl("royal", tolower(airlines$Airline))
+
+roy2 <- airlines[grepl("royal", airlines$Airline, ignore.case = TRUE), ]
+dim(roy2)
+roy2$Airline
+
