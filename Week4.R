ee71f5f6b8fce9b696631b71c72edce5d168f094
Dinh Nguyen <102342308+justdinhnq@users.noreply.github.com>
2023-03-24 23:00:59 +1300

    w4

--- Week4.R
+++ Week4.R
@@ -56,11 +56,50 @@ as.numeric(dd)
 Sys.time()
 # get day / time / timezone
 
+dd <- c("2023-07-04", "2023-03-14", "2023/02/28", "20 Mar 2023")
 
+# specify format
+ddl <- as.Date(dd, format="%Y-%m-%d")
+ddl
 
+format(ddl)
+format(ddl, format = "It's the date %d %B %Y")
 
+# date
+format(ddl, "%A")
 
+as.numeric(ddl)
 
+# Date-times- as extra subtlety
+# %H hourrs
+# %M minutes
+# %S seconds (continous - double)
 
+install.packages("lubridate")
+library(lubridate)
 
+ss <- c("1 Jan 1970 00:00:00", "23 Nov 1963 12:10:50")
+as.POSIXct(ss, format = "%d %B %Y %H:%M:%S")
 
+secs <- c(29, 10, 11, 20,55)
+mins <- c(0,0,13,34,59)
+hours <- c(0,1,1,1,12)
+ISOdate(2023, 3, 24, hours, mins, secs, tz="Pacific/Aukland")
+
+# arithmetic with dates
+class(ddl)
+ddl>as.Date("2015-1-1")
+
+mean(ddl)
+
+today <- Sys.Date()
+newyear <- as.numeric(format(today, "%Y"))-5
+as.Date(
+  paste(
+    newyear,
+    format(today, "-%m-%d"),
+    sep = ""
+  )
+)
+
+active <- read.csv("active-satellites.csv")
